2997e1ef63e448976709e88bd8c6db961aec890b
Magsat Ilzhanov <75102680+Ilzhanovm@users.noreply.github.com>
2021-03-24 15:09:04 +0300

    Minor code style changes

--- Chapter_1/task1_5.c
+++ Chapter_1/task1_5.c
@@ -6,7 +6,7 @@
 int printReverseFahrToCelScale() {
     printf("%5s%9s\n", "Fahr", "Cel");
 
-    for(int fahr = UPPER; fahr >= LOWER; fahr -= STEP)
+    for (int fahr = UPPER; fahr >= LOWER; fahr -= STEP)
         printf("%4d%4c%7.1f\n", fahr, '|' , (5.0 / 9.0) * (fahr - 32));
 
     return 0;
