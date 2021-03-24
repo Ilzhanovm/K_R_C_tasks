fb88328c929f201794936803ee61fe036f88f8c5
Magsat Ilzhanov <75102680+Ilzhanovm@users.noreply.github.com>
2021-03-24 14:31:20 +0300

    task 1.5 added

--- Chapter_1/task1_5.c
+++ Chapter_1/task1_5.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+#define lower 0
+#define upper 300
+#define step 20
 
-int task1_5() {
+int printReverseFahrToCelScale() {
+    printf("%5s%9s\n", "Fahr", "Cel");
+
+    for(int fahr = upper; fahr >= lower; fahr -= step)
+        printf("%4d%4c%7.1f\n", fahr, '|' , (5.0 / 9.0) * (fahr - 32));
 
     return 0;
 }
--- main.c
+++ main.c
@@ -3,12 +3,14 @@
 #include "Chapter_1/task1_2.c"
 #include "Chapter_1/task1_3.c"
 #include "Chapter_1/task1_4.c"
+#include "Chapter_1/task1_5.c"
 
 int main() {
     //helloWorld(); //task 1.1
     //symbolTest(); //task 1.2
     //printFahrToCelScale(); //task 1.3
-    printCelToFahrScale(); //task 1.4
+    //printCelToFahrScale(); //task 1.4
+    printReverseFahrToCelScale(); //task 1.5
 
     return 0;
 }
