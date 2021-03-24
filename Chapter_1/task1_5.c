9b1d5d340689e66948d5331cda7469efc3d92416
Magsat Ilzhanov <75102680+Ilzhanovm@users.noreply.github.com>
2021-03-24 14:37:20 +0300

    constant names changed

--- Chapter_1/task1_5.c
+++ Chapter_1/task1_5.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-#define lower 0
-#define upper 300
-#define step 20
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
 
 int printReverseFahrToCelScale() {
     printf("%5s%9s\n", "Fahr", "Cel");
 
-    for(int fahr = upper; fahr >= lower; fahr -= step)
+    for(int fahr = UPPER; fahr >= LOWER; fahr -= STEP)
         printf("%4d%4c%7.1f\n", fahr, '|' , (5.0 / 9.0) * (fahr - 32));
 
     return 0;
